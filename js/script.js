cf8192375a843c4d5341c2c8db0b87e26d810013
PeeperInTheSpace <lollof94@gmail.com>
2022-08-25 19:03:32 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ alert("Benvenuto, acquista il tuo biglietto!")
  
 // Dati vendita
 const CostoKM = 0.21; 
-let sconto = 0; 
-let scontoApplicato = 0; 
+let scontoApplicato = 0;
  
 // Dati da inserire
 const KMInseriti = parseInt(prompt ("Inserisci i KM da percorrere")); 
@@ -18,18 +17,14 @@ console.log(PrezzoBiglietto);
  
 // Condizione sconto
 if (EtaInserita < 18) { 
-    sconto = 20; 
-    console.log(sconto); 
  
-    scontoApplicato = (PrezzoBiglietto * sconto) / 100; 
+    scontoApplicato = (PrezzoBiglietto * 0.2); 
     console.log(scontoApplicato); 
 } 
 
 else if (EtaInserita > 65) { 
-    sconto = 40; 
-    console.log(sconto); 
  
-    scontoApplicato = (PrezzoBiglietto * sconto) / 100; 
+    scontoApplicato = (PrezzoBiglietto * 0.4); 
     console.log(scontoApplicato); 
 } 
  
